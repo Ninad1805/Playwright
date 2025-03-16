9ee0cfd0c269a98dbd1d4ce20c669c75ce5f21cc
Ninad Sawant <ninadsawant@gmail.com>
2025-03-17 01:12:45 +0530

    Response body interception using route method

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -4,6 +4,7 @@ const { APIUtils } = require('./utils/apiUtils.js');
 const loginPayload = { userEmail: "sedene9987@doishy.com", userPassword: "Welcome@123" };
 const orderIdPayload = { orders: [{ country: "India", productOrderedId: "67a8dde5c0d3e6622a297cc8" }] };
 let response;
+const fakePayload = { data: [], message: "No Orders" };
 
 test.beforeAll(async ({ }) => {
     const apiContext = await request.newContext();
@@ -78,22 +79,26 @@ test('Order without login', async ({ page }) => {
 });
 
 
-test('Get order id using API', async ({ page }) => {
+test.only('No orders exits', async ({ page }) => {
 
     page.addInitScript(value => {
         window.localStorage.setItem('token', value);
     }, response.token);
 
     await page.goto('https://rahulshettyacademy.com/client');
-    const rows = await page.locator("tbody tr");
-    await page.locator("//button[@routerlink='/dashboard/myorders']").click()
-    await page.locator("//tbody").waitFor();
-    for (let i = 0; i < await rows.count(); ++i) {
-        const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if (response.orderId.includes(rowOrderId)) {
-            await rows.nth(i).locator("button").first().click();
-            break;
-        }
-    }
-    expect(response.orderId.includes(await page.locator("div.col-text").textContent())).toBeTruthy();
+
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+        async route => {
+            //Intercept the response
+            //Send a fake response
+            const response = await page.request.fetch(route.request());
+            let body = JSON.stringify(fakePayload);
+            route.fulfill({
+                response,
+                body,
+            });
+        });
+    await page.locator("button[routerlink*='myorders']").click()
+    await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
+    console.log(await page.locator(".mt-4").textContent())
 });
